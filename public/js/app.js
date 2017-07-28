d0ba29e53de653bc4aba6179a1fc1f23b7534a58
jahnava <jahnavalmt@gmail.com>
2017-07-28 13:44:05 -0400

    added dependencies and basicc set up for tribute page

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".row").css("background-color", "white");
+  $(".center-block").css("background-color", "#D3D3D3");
+  $(".center-block").css("margin-top",50);
+  $("body").css("text-align","center");
+  $(".center-block").css("padding-top",10);
+  $("h1").css("font-size",63);
+  $("#box").css("background-color","white");
+
+
+});
