e61f823bfab03e7c35bfa06774a3db720415baae
jahnava <jahnavalmt@gmail.com>
2017-07-28 15:21:16 -0400

    added border around image and center box

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   $(".row").css("background-color", "white");
   $(".center-block").css("background-color", "#D3D3D3");
+  $(".center-block").css({"border-color": "#D3D3D3",
+             "border-weight":"1px",
+             "border-style":"solid",
+              "border-radius":'15px'});
   $(".center-block").css("margin-top",50);
   $("body").css("text-align","center");
   $(".center-block").css("padding-top",10);
   $("h1").css("font-size",63);
- $('.center-block').width('80%');
+  $('.center-block').width('80%');
+  $("figure-caption").css("background-color","white");
+  $("#white-box").css("background-color","white");
+  $("#white-box").css({"border-color": "silver",
+             "border-weight":"1px",
+             "border-style":"solid",
+              "border-radius":'15px'});
+
 });
